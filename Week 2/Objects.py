b66f2bccba1b17fe480ca2d98bfaf50e547c0c0a
arielleroyston <31903219+arielleroyston@users.noreply.github.com>
2017-10-04 18:10:56 -0400

    Create Objects.py

---
+++ Week 2/Objects.py
@@ -0,0 +1,53 @@
+#Dog
+myDict = {
+	"furLength": "Long",
+	"furColor": "White",
+	"breed": "Samoyed",
+	"tailType": "Long and curly",
+	"fixed": True,
+	"legCount": 4,
+	"temperment": "Happy / Playful",
+	"age": .7
+}
+
+#Bed
+myDict = {
+	"size": "Queen",
+	"pillowsIncluded": 5,
+	"comforter": True,
+	"comforterColor": "Grey",
+	"patternedComforter": True,
+	"mattressType": "Plush",
+	"sheetsIncluded": True,
+	"sheetColor": "White",
+	"bedframeType": "Platform"
+}
+
+#Salad
+myDict = {
+	"lettuce": True,
+	"cucumber": True,
+	"tomato": False,
+	"carrots": False,
+	"onion": True,
+	"croutons": True,
+	"asparagus": True,
+	"apples": True,
+	"heartsOfPalm": True,
+	"chicken": False,
+	"steak": True,
+	"cheese": False,
+	"olives": True,
+}
+
+#Sneaker
+myDict = {
+	"sneakerType": "Fashion",
+	"comfortLevel": "Low",
+	"sneakerSize": 7.5,
+	"brand": "Superga",
+	"soleSize": 1,
+	"soleMaterial": "Rubber",
+	"bodyMaterial": "Canvas",
+	"mainColor": "White"
+}
