0e22f0c03151547ad720b19a8d5acfd20efcf321
arielleroyston <31903219+arielleroyston@users.noreply.github.com>
2017-10-06 13:41:36 -0400

    Create Potent_p5_Sketch.js

---
+++ Week 5/Potent_p5_Sketch.js
@@ -0,0 +1,74 @@
+var serial;          // variable to hold an instance of the serialport library
+var portName = '/dev/cu.usbmodem1421';  // fill in your serial port name here
+var inData;   
+var xPos = 0; 
+
+function setup() {
+  serial = new p5.SerialPort();       // make a new instance of the serialport library
+  serial.on('list', printList);  // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+ 
+  serial.list();                      // list the serial ports
+  serial.open(portName);              // open a serial port
+	
+	createCanvas(400, 300);
+  background("black");
+
+}
+
+function draw() {
+	colorMode(HSB, 100);  // Use HSB with scale of 0-100
+	c = color(255-inData, 55, 100);
+	stroke(c);
+  text("sensor value: " + inData, 30, 30);
+	graphData(inData)
+}
+// get the list of ports:
+function printList(portList) {
+ // portList is an array of serial port names
+ for (var i = 0; i < portList.length; i++) {
+ // Display the list the console:
+ print(i + " " + portList[i]);
+ }
+}
+
+function serverConnected() {
+  print('connected to server.');
+}
+ 
+function portOpen() {
+  print('the serial port opened.')
+}
+ 
+function serialEvent() {
+  inData = Number(serial.read());
+}
+ 
+function serialError(err) {
+  print('Something went wrong with the serial port. ' + err);
+}
+ 
+function portClose() {
+  print('The serial port closed.');
+}
+
+function graphData(newData) {
+  // map the range of the input to the window height:
+  var yPos = map(newData, 0, 255, 0, height);
+  // draw the line in a pretty color:
+  //stroke(0xA8, 0xD9, 0xA7);
+  line(xPos, height, xPos, height - yPos);
+  // at the edge of the screen, go back to the beginning:
+  if (xPos >= width) {
+    xPos = 0;
+    // clear the screen by resetting the background:
+   background("black");
+  } else {
+    // increment the horizontal position for the next reading:
+    xPos++;
+  }
+}
